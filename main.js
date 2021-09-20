0d015ca7c3b4beb35d85c8465d300bb6e22dcbfb
prajak-star <80954908+prajak-star@users.noreply.github.com>
2021-09-20 16:53:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+}
+
+function setup(){
+    canvas=createCanvas(600,400);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,400);
+
+}
+ 
+function cutesy(){
+    save("freshlynosedimage.png");
+}
